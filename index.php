3c06a264a2d151e6a48a587b7e56183d53783c2e
algrvvv <alexandrgr25@gmail.com>
2024-01-27 12:01:04 +0300

    testing new functions №3

--- index.php
+++ index.php
@@ -12,6 +12,14 @@ require "./helper.php";
 //     [7]
 // ]);
 
+// $collection = collect([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]);
+
+// $collection->collapse()->print(); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 // $coll = collect(['name', 'age']);
 
 // $coll->combine(['sasha', 18]); //['name' => 'sasha', 'age' => 18]
@@ -53,5 +61,62 @@ require "./helper.php";
 // $collection->diff([2, 4, 6, 8])->print(); // [1,3,5]
 
 // $collection = collect(['a', 'b', 'a', 'c', 'b']);
- 
+
 // $collection->duplicates()->print(); // [2 => 'a', 4 => 'b']
+
+// $collection = collect([1, 2, 3, 4]);
+
+// $new = $collection->each(function (int $item, int $key) {
+//     if($item == 3){
+//         return false;
+//     }
+
+//     return $item;
+// });
+
+// $new->print(); // [0 => 1, 1 => 2]
+
+// $user = collect(["login" => "exampleLogin", "email" => "test@example.com"]);
+
+// $user->login; // exampleLogin
+// $user->email; // test@example.com
+
+// $collection = collect(['product_id' => 1, 'price' => 100, 'discount' => false]);
+
+// $collection->except(['price', 'discount'])->print(); // ['product_id' => 1]
+// $collection->except('price')->print(); // ['product_id' => 1, 'discount' => false]
+// $collection->except('test')->print(); // ['product_id' => 1, 'price' => 100, 'discount' => false]
+
+// $collection = collect([
+//     'product_id' => 1,
+//     'name' => 'Desk',
+//     'price' => 100,
+//     'discount' => false
+// ]);
+
+// $collection->only(['product_id', 'name'])->print(); // ['product_id' => 1, 'name' => 'Desk']
+
+// $collection = collect([1, 2, 3, 4]);
+
+// $collection->filter(function (int $value, int $key) {
+//     return $value > 2;
+// })->print(); // [3, 4]
+
+// collect([1, 2, 3, 4])->first(function (int $value, int $key) {
+//     return $value > 2;
+// }); // 3
+
+// collect([1, 2, 3, 4])->first(); // 1
+
+// $collection = collect(['name' => 'sasha', 'framework' => 'laravel']);
+
+// $collection->flip()->print(); // ['sasha' => 'name', 'laravel' => 'framework']
+
+// $collection = collect(['name' => 'taylor', 'framework' => 'laravel']);
+ 
+// $collection->forget('name')->print(); // ['framework' => 'laravel']
+
+$collection = collect(['name' => 'sasha', 'framework' => 'laravel']);
+ 
+$value = $collection->get('name'); // 'sasha'
+$value = $collection->get('nameFramework', "laravel"); // 'laravel'
